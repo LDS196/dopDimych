b87262f49a8dd20d624f23e88d7db2bb74c1ae78
LDS196 <lihachevd87@gmail.com>
2023-02-25 23:38:31 +0400

    refactor UseEffect.stories.tsx

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -9,11 +9,11 @@ export default {
 export const Example1 = () => {
     console.log('ex1 ')
     const [counter, setCounter] = useState(0)
-useEffect(()=>{
-    document.title= counter.toString()
-})
+    useEffect(() => {
+        document.title = counter.toString()
+    })
     return <>
-        <button onClick={() => setCounter(state=> state +1)}>+</button>
+        <button onClick={() => setCounter(state => state + 1)}>+</button>
         {counter}
     </>
 }
@@ -21,14 +21,37 @@ useEffect(()=>{
 export const SetTimeOutExample = () => {
     console.log('ex1 ')
     const [counter, setCounter] = useState(0)
-    useEffect(()=>{
-        setInterval(()=>{
-           setCounter(state=> state + 1)
-        },1000)
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(state => state + 1)
+        }, 1000)
 
-    },[])
+    }, [])
     return <>
         {/*<button onClick={() => setCounter(state=> state +1)}>+</button>*/}
         {counter}
     </>
+}
+
+export const ExampleKeysTracker = () => {
+
+    const [text, setText] = useState('')
+    console.log('render with' + text)
+
+
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            setText(text + e.key)
+        }
+        window.document.addEventListener('keypress', handler)
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+    },[text]
+)
+return <>
+    Type:{text}
+
+</>
 }
